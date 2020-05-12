0068a5cc727860ce4aa791945dea96843e3e04a0
Christopher Keith <chris.keith@gmail.com>
2020-05-12 11:14:57 -0700

    First code commit.

---
+++ src/jeff-light-sensor.ino
@@ -0,0 +1,174 @@
+
+const String githubHash = "to be replaced manually (and code re-flashed) after 'git push'";
+
+#include <limits.h>
+
+class JSonizer {
+  public:
+    static void addFirstSetting(String& json, String key, String val);
+    static void addSetting(String& json, String key, String val);
+    static String toString(bool b);
+};
+
+class Utils {
+  public:
+    static const int publishRateInSeconds;
+    static bool publishDelay;
+    static int setInt(String command, int& i, int lower, int upper);
+    static void publish(String event, String data);
+    static void publishJson();
+    static String getName();
+};
+
+class TimeSupport {
+  private:
+    unsigned long ONE_DAY_IN_MILLISECONDS;
+    unsigned long lastSyncMillis;
+    String timeZoneString;
+    String getSettings();
+  public:
+    int timeZoneOffset;
+    TimeSupport(int timeZoneOffset, String timeZoneString);
+    String timeStrZ(time_t t);
+    String nowZ();
+    void handleTime();
+    int setTimeZoneOffset(String command);
+    void publishJson();
+};
+
+void JSonizer::addFirstSetting(String& json, String key, String val) {
+    json.concat("\"");
+    json.concat(key);
+    json.concat("\":\"");
+    json.concat(val);
+    json.concat("\"");
+}
+
+void JSonizer::addSetting(String& json, String key, String val) {
+    json.concat(",");
+    addFirstSetting(json, key, val);
+}
+
+String JSonizer::toString(bool b) {
+    if (b) {
+        return "true";
+    }
+    return "false";
+}
+
+const int Utils::publishRateInSeconds = 5;
+bool Utils::publishDelay = true;
+int Utils::setInt(String command, int& i, int lower, int upper) {
+    int tempMin = command.toInt();
+    if (tempMin > lower && tempMin < upper) {
+        i = tempMin;
+        return 1;
+    }
+    return -1;
+}
+void Utils::publish(String event, String data) {
+    Particle.publish(event, data, 1, PRIVATE);
+    if (publishDelay) {
+      delay(1000);
+    }
+}
+void Utils::publishJson() {
+    String json("{");
+    JSonizer::addFirstSetting(json, "githubHash", githubHash);
+    JSonizer::addSetting(json, "githubRepo", "to come");
+    JSonizer::addSetting(json, "publishRateInSeconds", String(publishRateInSeconds));
+    JSonizer::addSetting(json, "publishDelay", JSonizer::toString(publishDelay));
+    json.concat("}");
+    publish("Utils json", json);
+}
+String Utils::getName() {
+  String id = System.deviceID();
+  String location = "Unknown";
+  if (id.equals("1f0027001347363336383437")) {
+    location = "Light sensor";
+  }
+  return location;
+}
+
+String TimeSupport::getSettings() {
+    String json("{");
+    JSonizer::addFirstSetting(json, "lastSyncMillis", String(lastSyncMillis));
+    JSonizer::addSetting(json, "timeZoneOffset", String(timeZoneOffset));
+    JSonizer::addSetting(json, "timeZoneString", String(timeZoneString));
+    JSonizer::addSetting(json, "internalTime", nowZ());
+    json.concat("}");
+    return json;
+}
+
+TimeSupport::TimeSupport(int timeZoneOffset, String timeZoneString) {
+    this->ONE_DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
+    this->lastSyncMillis = millis();
+    this->timeZoneOffset = timeZoneOffset;
+    this->timeZoneString = timeZoneString;
+    Time.zone(timeZoneOffset);
+    Particle.syncTime();
+    lastSyncMillis = millis();
+}
+
+String TimeSupport::timeStrZ(time_t t) {
+    String fmt("%a %b %d %H:%M:%S ");
+    fmt.concat(timeZoneString);
+    fmt.concat(" %Y");
+    return Time.format(t, fmt);
+}
+
+String TimeSupport::nowZ() {
+    return timeStrZ(Time.now());
+}
+
+void TimeSupport::handleTime() {
+    if (millis() - lastSyncMillis > ONE_DAY_IN_MILLISECONDS) {    // If it's been a day since last sync...
+                                                            // Request time synchronization from the Particle Cloud
+        Particle.syncTime();
+        lastSyncMillis = millis();
+    }
+}
+
+int TimeSupport::setTimeZoneOffset(String command) {
+    timeZoneString = "???";
+    return Utils::setInt(command, timeZoneOffset, -24, 24);
+}
+
+void TimeSupport::publishJson() {
+    Utils::publish("TimeSupport", getSettings());
+}
+TimeSupport    timeSupport(-8, "PST");
+
+// getSettings() is already defined somewhere.
+int pubSettings(String command) {
+    if (command.compareTo("") == 0) {
+        Utils::publishJson();
+    } else if (command.compareTo("time") == 0) {
+        timeSupport.publishJson();
+    } else {
+        Utils::publish("GetSettings bad input", command);
+    }
+    return 1;
+}
+
+int publishData(String command) {
+  int value = analogRead(A0);
+  String s(value);
+  Particle.publish("Light sensor", s.c_str(), PRIVATE);
+  return 1;
+}
+
+void setup() {
+  Particle.publish("Debug", "Started setup...", 1, PRIVATE);
+  Particle.function("publishData", publishData);
+  Particle.function("getSettings", pubSettings);
+
+  pinMode(A0, INPUT);
+  publishData("");
+  Particle.publish("Debug", "Finished setup...", 1, PRIVATE);
+}
+
+void loop() {
+  publishData("");
+  delay(3000);
+}
